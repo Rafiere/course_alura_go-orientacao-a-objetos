dd16679c2e50629b19ab9622c650af6330e6d824
Rafiere <rafapgbr@gmail.com>
2023-04-11 08:07:19 -0300

    feat: add lesson 02 code

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type ContaCorrente struct {
 	titular       string
 	numeroAgencia int
 	numeroConta   int
 	saldo         float64
+}
+
+/* O "conta *ContaCorrente diz que, quando usarmos o método "sacar", ele referenciará o
+objeto que chamar essa função, semelhante ao "this" do Java. */
+
+func (conta *ContaCorrente) sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque <= conta.saldo
+
+	if podeSacar {
+		conta.saldo -= valorDoSaque
+		return "O saque foi realizado com sucesso."
+	} else {
+		return "Saldo Insuficiente!"
+	}
 }
 
 func main() {
@@ -27,4 +44,19 @@ func main() {
 	}
 
 	fmt.Println(contaDoRafa)
+	fmt.Println(reflect.TypeOf(contaDoRafa))
+
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+
+	/* Quando usamos o &variavel, estamos acessando o endereço dessa variável. Quando
+	usamos o "*variavel", estamos acessando o valor que está nesse endereço. A sintaxe
+	"*variavel" só é válida para acessarmos o valor em que o ponteiro está apontando. */
+
+	fmt.Println(reflect.TypeOf(*contaDaCris))
+
+	fmt.Println(contaDoRafa.saldo)
+	fmt.Println(contaDoRafa.sacar(430))
+	fmt.Println("Novo valor: R$", contaDoRafa.saldo)
 }
